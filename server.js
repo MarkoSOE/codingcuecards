cd5f91267c2bdd1931a2ebbbcaeba40c3669e488
Marko Alhamadani <Markoalhamadani@gmail.com>
2022-07-13 22:44:49 -0400

    .

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const MongoClient = require('mongodb').MongoClient
 const app = express();
 const PORT = 3001;
 require('dotenv').config()
+
+let dbName = 'cuecard'
 let dbConnectionString = process.env.DB_STRING
 
 app.set('views', __dirname + '/views/')
@@ -15,7 +17,7 @@ app.use(express.static(__dirname + '/public'))
 MongoClient.connect(dbConnectionString, {useUnifiedTopology : true})
     .then(client => {
         console.log('Connected to database')
-        const db = client.db('cuecard')
+        const db = client.db(dbName)
         app.listen(PORT, () => {
             console.log(`Server running on port ${PORT}`)
         })
