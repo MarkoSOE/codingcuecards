222a2a76dfe308d185271089a88375cb0a5cba95
Marko Alhamadani <Markoalhamadani@gmail.com>
2022-07-16 23:12:32 -0400

    latest working

--- server.js
+++ server.js
@@ -14,35 +14,36 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'))
 
 let db,
+db2,
 dbConnectionStr = process.env.DB_STRING,
 dbName = 'cuecard'
 
 MongoClient.connect(dbConnectionStr, {useUnifiedTopology : true})
     .then(client => {
         console.log('Connected to database')
-        db = client.db(dbName)
-        const dbcollection = client.db('cuecard').collection('cuecardquestions')
-
-        app.get('/getQuestions', async (request, response) =>{
-            try{
-                const data = await dbcollection.find().toArray()
-                //select random question
-                function randomIntFromInterval(min, max) { // min and max included 
-                    return Math.floor(Math.random() * (max - min + 1) + min)
-                    }
-                const rndInt = randomIntFromInterval(0, data.length-1)
-                response.render('index.ejs', { info:[data[rndInt]]})
-            }
-            catch(error){
-                response.status(500).json({message: error.message})
-            }
-        })
-        
-        app.get('/', (request, response) => {
-            response.sendFile(__dirname + '/index.html')
-        })
+        db2 = client.db(dbName)
     }).catch(error => console.error(error))
 
-app.listen(process.env.PORT || PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})    
+    app.get('/getQuestions', async (request, response) =>{
+        try{
+            const data = await db2.collection('cuecardquestions').find().toArray()
+            //select random question
+            function randomIntFromInterval(min, max) { // min and max included 
+                return Math.floor(Math.random() * (max - min + 1) + min)
+                }
+            const rndInt = randomIntFromInterval(0, data.length-1)
+            response.render('index.ejs', { info:[data[rndInt]]})
+        }
+        catch(error){
+            response.status(500).json({message: error.message})
+        }
+    })
+    
+    app.get('/', (request, response) => {
+        response.sendFile(__dirname + '/index.html')
+    })
+
+
+    app.listen(process.env.PORT || PORT, () => {
+        console.log(`Server running on port ${PORT}`)
+    })      
